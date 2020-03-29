92c2f3d1551fc35014f8b81f6d9d8768431546bb
Leandro F Takahashi <leandro.fariatakahashi@gmail.com>
2020-03-29 13:41:24 -0300

    Front-end do app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React /*, { useState }*/ from 'react';
+
+//import Header from './Header';
+
+import './global.css';
+
+//import Logon from './pages/Logon';
+import Routes from './routes';
 
 function App() {
+  // const [counter, setCounter] = useState(0);
+  // useState retorna um Array com duas posições: [ valor, funcaoDeAtualizacao ]
+
+  //function increment(){
+  //  setCounter (counter + 1);
+  //}
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          Hello OmniStack!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //<div>
+    //  <Header>Contador: {counter}</Header>
+    //  <button onClick={increment}>Incrementar</button>
+    //</div>
+
+    <Routes />
   );
 }
 
